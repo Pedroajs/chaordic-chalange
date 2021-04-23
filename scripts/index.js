a5c9e9ac3b70a862af2d533363ee366715fa5cb3
Pedro Augusto <54971305+Pedroajs@users.noreply.github.com>
2021-04-23 05:15:29 -0300

    função de requisição a API

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+const url = "https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=1"
+
+function getApi(){
+   return axios.get(url)
+            .then(response => response.data.products)
+}
+
+async function criaCards(){
+    const arrayDeProdutos = await getApi();
+    const cardContainer = document.querySelector(".card-container");
+  
+
+    const listaDeProdutos = arrayDeProdutos.reduce((accumulator, element)=>{
+        accumulator += ` 
+            <li class="card">
+                <img src="${element.image}" alt="">
+                <p>${element.name}</p>
+                <p>${element.description}</p>
+                <span>de:${element.oldPrice}</span>
+                <span>por: ${element.price}</span>
+
+                <span>ou 2x de 0,50 centavos</span>
+                <button>comprar</button>
+            </li>
+        `
+        return accumulator
+    }, '')
+
+    cardContainer.innerHTML = listaDeProdutos;
+}
+criaCards();
+getApi();
